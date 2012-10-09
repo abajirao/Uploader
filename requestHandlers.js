0be31d4b8036643d1bb5dbd59ef399bf1ffb7ffb
Aniruddh Bajirao <abajirao@gmail.com>
2012-10-08 20:36:38 -0500

    demonstrating the advantages of using non-blocking io over block io

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,20 @@
-function start(){
+var exec = require("child_process").exec;
+
+function start(response){
 	console.log("Request handler 'start' was called. ");
+
+	exec ("ls -lah", function(error, stdout, stderr)){
+		response.writeHead(200, {"Content-type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	}
 }
 
-function upload(){
+function upload(response){
 	console.log("Request handler 'upload' was called. ");
+	response.writeHead(200, {"Content-type":"text/plain"});
+	response.write("Upload!!!");
+	response.end();
 }
 
 exports.start = start
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ function start(route, handle){
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received");
-		route(handle, pathname);
+		route(handle, pathname, response);
 
-		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write("\n Hello World \n");
-		response.end();
+		// response.writeHead(200,{"Content-Type":"text/plain"});
+		// response.write("\n Hello World \n");
+		// response.end();
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log(" \n Server has started... ");
