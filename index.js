03f1146f6af8c8be3b4e212ef57e05ab02a79aa9
Aniruddh Bajirao <abajirao@gmail.com>
2012-10-08 20:17:13 -0500

    Added the router and requestHandlers

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
 //import the server.js module
-var server = require("./server");
-server.start();
+var server = require ("./server");
+var router = require ("./router");
+var requestHandlers = require ("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
