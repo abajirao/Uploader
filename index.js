4f0bcd73d4b8260be5d5e80af0a28a03c4fd5546
Aniruddh Bajirao <abajirao@gmail.com>
2012-10-09 21:03:22 -0500

    Added code to handle image upload based on a node library called node-formidable

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 
-//import the server.js module
+// //import the server.js module
 var server = require ("./server");
 var router = require ("./router");
 var requestHandlers = require ("./requestHandlers");
+var formidable = require('formidable');
 
 var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
+
+// var formidable = require('formidable'),
+//     http = require('http'),
+//     sys = require('sys');
+
+// http.createServer(function(req, res) {
+//   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+//     // parse a file upload
+//     var form = new formidable.IncomingForm();
+//     form.parse(req, function(err, fields, files) {
+//       res.writeHead(200, {'content-type': 'text/plain'});
+//       res.write('received upload:\n\n');
+//       res.end(sys.inspect({fields: fields, files: files}));
+//     });
+//     return;
+//   }
+
+//   // show a file upload form
+//   res.writeHead(200, {'content-type': 'text/html'});
+//   res.end(
+//     '<form action="/upload" enctype="multipart/form-data" '+
+//     'method="post">'+
+//     '<input type="text" name="title"><br>'+
+//     '<input type="file" name="upload" multiple="multiple"><br>'+
+//     '<input type="submit" value="Upload">'+
+//     '</form>'
+//   );
+// }).listen(8888);
